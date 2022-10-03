c0191e9c5edb95705bbbf3a90368a05bd3e752cf
qclaytor30 <qclaytor30@gmail.com>
2022-10-03 16:49:02 -0400

    add delete function and fs to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,19 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-// GET Route for retrieving all the notes
+// GET Route for retrieving all notes
 notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes.`);
-
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+    
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.error('Error getting notes.');
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
 
 // POST Route for a new note
@@ -19,14 +26,62 @@ notes.post('/', (req, res) => {
         const newNote = {
             title,
             text,
-            note_id: uuidv4(),
+            note_id: uuidv4()
         };
 
-        readAndAppend(newNote, '../db/db.json');
-        res.json(`Note added successfully.`);
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.error('Error getting notes.');
+            } else {
+                const notesDatabase = JSON.parse(data);
+                notesDatabase.push(newNote);
+
+                fs.writeFile('./db/db.json', JSON.stringify(notesDatabase, null, 4), (err) => {
+                    if (err) {
+                        console.error(err);
+                        res.error('Error adding note.');
+                    } else {
+                        res.json(`Note added successfully.`);
+                    }
+                });
+            }
+        });
     } else {
         res.error('Error adding note.');
     }
 });
+
+// DELETE Route for removing a note
+notes.delete('/:ID', (req, res) => {
+    console.info(`${req.method} request received to delete a note.`);
+
+    const deletedNoteRequest = req.params.ID;
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.error('Error getting notes.');
+        } else {
+            const existingNotesDatabase = JSON.parse(data);
+            const newNotesDatabase = [];
+
+            for (let i = 0; i < existingNotesDatabase.length; i++) {
+                if (deletedNoteRequest !== existingNotesDatabase[i].note_id) {
+                    newNotesDatabase.push(existingNotesDatabase[i]);
+                    }
+                }
+
+        fs.writeFile('./db/db.json', JSON.stringify(newNotesDatabase, null, 4), (err) => {
+            if (err) {
+                console.error(err);
+                res.error('Error deleting note.');
+            } else {
+                res.json(`Note deleted successfully.`);
+            }
+        });
+    }
+        });
+})
 
 module.exports = notes;
