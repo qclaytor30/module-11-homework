9e05b546e1ba6ba87272cb7463709fc89fe29116
qclaytor30 <qclaytor30@gmail.com>
2022-10-03 16:19:00 -0400

    setup modular routing

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+// Import modular router for /notes
+const notesRouter = require('./notes');
+const app = express();
+app.use('/notes', notesRouter);
+
+module.exports = app;
