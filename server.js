19221c3c716c06a0917b2a276f5b853564d94c66
qclaytor30 <qclaytor30@gmail.com>
2022-12-19 21:18:58 -0500

    readme

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const api = require('./routes/index');
 const PORT = process.env.PORT || 3001;
-const app = express();
 const express = require('express');
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
-
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) =>
